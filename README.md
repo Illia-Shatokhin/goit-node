4a84ecc96ecde41e192d95a018fcbe4760dabe84
Illia Shatokhin <resonane.2106@gmail.com>
2023-08-29 20:45:57 +0300

    add branch with first finish first hw

---
+++ README.md
@@ -0,0 +1,4 @@
+https://ibb.co/4Z4XCPJ
+https://ibb.co/cQ8KN3j
+https://ibb.co/n3hPk4s
+https://ibb.co/XWQdp8N
--- contacts.js
+++ contacts.js
@@ -21,12 +21,9 @@ export async function getContactById(contactId) {
 export async function removeContact(contactId) {
   const contacts = await listContacts();
   const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) {
-    return null;
-  }
+  if (index === -1) return null;
   const [result] = contacts.splice(index, 1);
   await updateContacts(contacts);
-  console.log(result);
   return result;
 }
 
